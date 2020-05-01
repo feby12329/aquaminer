c9efe0afe9c708b105b842548ad4c18f4641bc8e
aerth <aerth@riseup.net>
2020-05-01 03:58:23 +0000

    between building releases, clean spdlog too
    this may fix the travis-ci binary releases,
    where i found a vpxor op in a non-avx binary.
    
    adds some time to the release compilation.

--- scripts/build_release.bash
+++ scripts/build_release.bash
@@ -1,18 +1,23 @@
 set -ex
 mkdir aquachain-miner # should not exist
 # clean everything
-make clean && make -C aquahash clean
+cleanall(){
+  make clean
+  make -C aquahash clean
+  make -C spdlog clean
+}
+cleanall
 
 # build each, copy to folder and clean afterwards
 make -j 4 config=plain
 mv bin/* aquachain-miner/
-make clean && make -C aquahash clean
+cleanall
 make -j 4 config=avx
 mv bin/* aquachain-miner/
-make clean && make -C aquahash clean
+cleanall
 make -j 4 config=avx2
 mv bin/* aquachain-miner/
-make clean && make -C aquahash clean
+cleanall
 
 tar czvf aquachain-miner-linux-amd64.tar.gz aquachain-miner/
 ls -thalr aquachain-miner-linux-amd64.tar.gz
